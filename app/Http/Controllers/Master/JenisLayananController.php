5db36dce72084dbd60c3c60be5d190fa1860ceb3
Gilang Raka Ramadhan <gilangrakar@gmail.com>
2024-11-20 15:27:25 +0700

    Add controller master jenis layanan

--- app/Http/Controllers/Master/JenisLayananController.php
+++ app/Http/Controllers/Master/JenisLayananController.php
@@ -2,34 +2,71 @@
 
 namespace App\Http\Controllers\Master;
 
+use App\Http\Controllers\BaseController;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\JenisLayananRequest;
+use App\Models\RefJenisLayanan;
 use Illuminate\Http\Request;
 
-class JenisLayananController extends Controller
+class JenisLayananController extends BaseController
 {
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        try {
+            $data = RefJenisLayanan::all();
+            return $this->sendResponse($data);
+        } catch (\Exception $e) {
+            return $this->sendError($e->getMessage(), 500);
+        }
+    }
+
+    public function Show($id)
+    {
+        try {
+            $data = RefJenisLayanan::find($id);
+            if (!$data) return $this->sendError('Jenis layanan tidak ditemukan!');
+
+            return $this->sendResponse($data);
+        } catch (\Exception $e) {
+            return $this->sendError($e->getMessage(), 500);
+        }
     }
 
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(JenisLayananRequest $request)
     {
-        //
+        try {
+            $data = new RefJenisLayanan([
+                'nama' => $request->nama
+            ]);
+            $data->save();
+
+            return $this->sendResponse($data);
+        } catch (\Exception $e) {
+            return $this->sendError($e->getMessage(), 500);
+        }
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(JenisLayananRequest $request, string $id)
     {
-        //
+        try {
+            $data = RefJenisLayanan::find($id);
+            if (!$data) return $this->sendError('Jenis layanan tidak ditemukan');
+
+            $data->update($request->validated());
+            return $this->sendResponse($data);
+        } catch (\Exception $e) {
+            return $this->sendError($e->getMessage(), 500);
+        }
     }
 
     /**
@@ -37,6 +74,14 @@ class JenisLayananController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        try {
+            $data = RefJenisLayanan::find($id);
+            if (!$data) return $this->sendError('Jenis layanan tidak ditemukan');
+
+            $data->delete();
+            return $this->sendResponse(null);
+        } catch (\Exception $e) {
+            return $this->sendError($e->getMessage(), 500);
+        }
     }
 }
